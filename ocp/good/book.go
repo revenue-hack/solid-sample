05db589d6d09b5b3a9fbf97e20f82a4a179804ab
revenue-hack <K.odeveloper10@gmail.com>
2021-09-05 22:09:29 +0900

    feat: add comment for ocp

--- ocp/good/book.go
+++ ocp/good/book.go
@@ -26,5 +26,17 @@ func (b *Book) PublishDate() time.Time {
 // 色々な本が増えていくたびにこの関数のロジックが追加される(修正に対して閉じていない)
 // 本の種類が増えても修正する必要がなくなった
 func (b *Book) printRentalType() string {
-	return fmt.Sprintf("これは%sです", b.kind.String())
+	return fmt.Sprintf("これは%sです\n", b.kind.String())
+}
+
+// [追加仕様]
+// もしprintAllOfRentalTypeで表示順番をドメイン知識として制御したい(つまりこの関数の中で制御したい)
+// この仕様に対してOCPは準拠していない(printRentalTypeの抽象化の意味がなかった)
+// 結論: あらゆる変更に対して完璧に閉じることは不可能。設計担当者はどういった種類の変更が頻繁に起こるのかを予測しそれに対してOCP準拠しておくこと
+func printAllOfRentalType(books []*Book) []string {
+	prints := make([]string, len(books))
+	for i, b := range books {
+		prints[i] = b.printRentalType()
+	}
+	return prints
 }
